e80273530994b17d76854bea67ffbd249e23b249
mes1234 <witold.kepczynski@gmail.com>
2018-08-15 11:16:44 +0200

    unification of returns with frontend

--- fnStore/generic.py
+++ fnStore/generic.py
@@ -1,4 +1,4 @@
-
+import inspect
 class FnStore():
     '''
 
@@ -15,11 +15,17 @@ class FnStore():
         '''
         register function and add it to fnStore
         '''
+        #TODO add units 
         fn = {}
+        args= inspect.getargspec(fnct)
         fn['description']= fnct.__code__.co_consts
-        fn['name']=fnct.__code__.co_name
+        fn['group']=fnct.__code__.co_name.split("_")[0]
+        fn['name']=fnct.__code__.co_name.split("_")[1]
+        fn['id']=len(self.fnStore)
         fn['argCount']= fnct.__code__.co_argcount
-        fn['args']= fnct.__code__.co_varnames[:fnct.__code__.co_argcount]
+        fn['args']= args.args
+        fn['default']=args.defaults
+        fn['types']=list(map(lambda x:str(type(x)).split("'")[1],args.defaults))
         fn['exec']=fnct
         self.fnStore.append(fn)
     def __str__(self):
@@ -33,9 +39,42 @@ class FnStore():
         '''
         pass
     def getArgs(self,name):
+        '''
+        TODO make return stgh like in values
+                    selected: {
+                        id:1,
+                        group:'arytmetyka',
+                        name:'dodaj'
+                    },
+                    values:{
+                        'pierwsza': {
+                            valueUnit: '%',
+                            value: 1.,
+                            valueType: 'number'
+                        },
+                        'druga': {
+                            valueUnit: 'lbm',
+                            value: 'Artur',
+                            valueType: 'text'
+                        },
+                        'trzecia': {
+                            valueUnit: '%',
+                            value: 3.,
+                            valueType: 'number'
+                        },
+                    }, // values to be passed to server every item has key is name, valueUnit, valueDefault, valueType
+                    description: 'dodawanie trzech liczb', //string to describe tool
+                    result:{}
+                }
+        '''
+        values= {} # dict with currently
+        # args= list(list(filter(lambda x: x['name']==name,self.fnStore))[0]['args'])
+        # selected['id']=tool.['id']
+        #  selected['group']=tool.['group']
+        #   selected['name']=tool.['name']
         return list(list(filter(lambda x: x['name']==name,self.fnStore))[0]['args'])
     def listFn(self):
         '''
         returns list with names of all functions
         '''
-        return list(map(lambda x:x['name'],self.fnStore))
+        return list(map(lambda x:{'id':x['id'],'group':x['group'],'name':x['name']},self.fnStore))
--- tests/test_FnStore.py
+++ tests/test_FnStore.py
@@ -3,14 +3,14 @@ import pytest
 '''
 dummy function for test
 '''
-def add(x=1,y=1):
+def arytmetyka_add(x=1,y=1):
     '''
     function to calculate sum of two elements
     '''
     res = x+y
     return res
 
-def diff(x=1,y=1):
+def arytmetyka_diff(x=1,y=1):
     '''
     function to calculate difference of two elements
     '''
@@ -22,8 +22,8 @@ create Store object
 @pytest.fixture(scope='session') 
 def Store():
     Store =FnStore() # create store
-    Store.register(add) # add dummy function
-    Store.register(diff) # add dummy function
+    Store.register(arytmetyka_add) # add dummy function
+    Store.register(arytmetyka_diff) # add dummy function
     return Store
  ################################
 ######       TESTS       #########
@@ -32,9 +32,23 @@ def test_StoreFnctListing(Store):
     '''
     test correctness of fetching list of fncts
     '''
-    assert Store.listFn() == ['add','diff']
+    assert Store.listFn() == [
+        {
+            'id':0,
+            'group':'arytmetyka',
+            'name':'add'
+        },
+       {
+            'id':1,
+            'group':'arytmetyka',
+            'name':'diff'
+        },
+    ]
+
+
 def test_StoreFnctArgListing(Store):
     '''
     test correctness of fetching list of args for fncts
     '''
+    #TODO setup test to run flawlessly with VUE
     assert Store.getArgs('add') == ['x','y']
