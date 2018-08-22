1dd8796a3a877dcb5cddedfa22f68a9b644a3d09
mes1234 <witold.kepczynski@gmail.com>
2018-08-22 22:55:49 +0200

    bez PL znakow

--- fnStore/geometria.py
+++ fnStore/geometria.py
@@ -1,10 +1,10 @@
 def geometria_pitagoras(x_=1.,y_=1.):
     '''
-    *oblicz równanie Pitagorasa*
+    *oblicz rownanie Pitagorasa*
     '''
     return x_*x_+y_*y_
 def geometria_poleKola(d_=1.):
     '''
-    *oblicz pole koła*
+    *oblicz pole kola*
     '''
     return 3.1415*d_*d_/4.
