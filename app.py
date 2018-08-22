6934b6b05d9208158c805973ea531f6882ff9ce1
mes1234 <witold.kepczynski@gmail.com>
2018-08-22 22:44:24 +0200

    added cors support

--- app.py
+++ app.py
@@ -4,9 +4,16 @@ from flask import Flask, request, g, jsonify
 from fnStore.generic import FnStore
 from fnStore.arytmetyka import *
 from fnStore.geometria import *
+from flask_cors import CORS
+
+
+
 
 app= Flask(__name__)
+CORS(app)
+
 store=FnStore()
+# TODO add method to add all of functions in module
 store.register(arytmetyka_dodaj)
 store.register(arytmetyka_odejmij)
 store.register(arytmetyka_podziel)
@@ -27,9 +34,16 @@ def getParams(tool_id):
     return list with all parameters for selected tool
     '''
     return jsonify(store.getArgs(tool_id))
+@app.route("/getDesc/<int:tool_id>",methods=['GET'])
+def getDesc(tool_id):
+    '''
+    return description for selected tool
+    '''
+    return jsonify(store.getDesc(tool_id))
+    # TODO add calculate route
 if __name__ == '__main__':
     app.debug = True
     # app.config['DATABASE_NAME'] = 'library.db'
     host = os.environ.get('IP', '0.0.0.0')
-    port = int(os.environ.get('PORT', 8080))
+    port = int(os.environ.get('PORT', 8081))
     app.run(host=host, port=port)
