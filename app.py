f16924ee58beaf6f0430fb713ee36193f00aa995
mes1234 <witold.kepczynski@gmail.com>
2018-08-28 22:30:48 +0200

    pythonanywhere deploy v1

--- app.py
+++ app.py
@@ -1,16 +1,26 @@
 import os
 
-from flask import Flask, request, g, jsonify
+from flask import Flask, request, g, jsonify, render_template
 from fnStore.generic import FnStore
 from fnStore.arytmetyka import *
 from fnStore.geometria import *
-from flask_cors import CORS
+# from flask_cors import CORS
+
+class VueFlask(Flask):
+  jinja_options = Flask.jinja_options.copy()
+  jinja_options.update(dict(
+    block_start_string='(%',
+    block_end_string='%)',
+    variable_start_string='((',
+    variable_end_string='))',
+    comment_start_string='(#',
+    comment_end_string='#)',
+  ))
 
 
 
-
-app= Flask(__name__)
-CORS(app)
+app= VueFlask(__name__)
+# CORS(app)
 
 store=FnStore()
 # TODO add method to add all of functions in module
@@ -21,7 +31,12 @@ store.register(arytmetyka_pomnoz)
 store.register(geometria_pitagoras)
 store.register(geometria_poleKola)
 
-
+@app.route("/",methods=['GET'])
+def serveApp():
+    '''
+    serve Vue app
+    '''
+    return render_template('index.html')
 @app.route("/getList",methods=['GET'])
 def getList():
     '''
