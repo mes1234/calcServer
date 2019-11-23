2ae172f528357f74869fa566841632d3c2031061
mes1234 <witold.kepczynski@gmail.com>
2019-11-23 21:41:34 +0100

    Update app.py

--- app.py
+++ app.py
@@ -108,5 +108,5 @@ if __name__ == '__main__':
     app.debug = False
     # app.config['DATABASE_NAME'] = 'library.db'
     host = os.environ.get('IP', '0.0.0.0')
-    port = int(os.environ.get('PORT', 8081))
+    port = int(os.environ.get('PORT', 5000))
     app.run(host=host, port=port)
